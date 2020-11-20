da55ecbba5c297fad19e9ecb17017abd10438a04
Bartosz <b.lodek@softwarepartner.pl>
2020-11-20 18:06:43 +0100

    wip uart service

--- src/UARTService.ts
+++ src/UARTService.ts
@@ -2,6 +2,7 @@ import { Serial } from "raspi-serial";
 
 export class UARTService {
   serial = null;
+  message = "";
   constructor() {
     this.serial = new Serial({ portId: "/dev/serial0" });
     this.serial.open(() => {
@@ -14,7 +15,17 @@ export class UARTService {
   };
 
   private onData = (data) => {
-    console.log(data);
+    const partOfMessage = String(data);
+    if (partOfMessage.charAt(0) !== "{" && this.message.charAt(0) !== "{") {
+      return;
+    } else {
+      this.message += partOfMessage;
+    }
+    if (this.message.slice(-1) === "}") {
+      console.log("done", this.message);
+      this.message = "";
+    }
+    console.log("wip", this.message);
     // const { s1, s2 } = JSON.parse(data);
     // console.log({ s1, s2 });
   };
